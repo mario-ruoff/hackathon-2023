6f7cbf2871dde6fd57a054d90ecfe11610a0568a
Mario Ruoff <st175600@stud.uni-stuttgart.de>
2023-05-07 00:42:01 +0200

    Add beeping

--- beep_me/lib/main.dart
+++ beep_me/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:audioplayers/audioplayers.dart';
+import 'dart:convert';
 import 'dart:async';
 
 void main() async {
@@ -32,18 +33,19 @@ class MainPage extends StatefulWidget {
 }
 
 class MainPageState extends State<MainPage> {
-  final _cameraFrequency = 0.5;
-  final endpointUrl = 'https://einsteamyolo.free.beeceptor.com';
+  final _cameraFrequency = 0.2;
+  final endpointUrl = 'https://lololololololol.free.beeceptor.com';
   late CameraController _controller;
   late Future<void> _initializeControllerFuture;
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  Timer _beepTimer = Timer(Duration.zero, () {});
+  double _beepFrequency = 0;
 
   @override
   void initState() {
     super.initState();
     _controller = CameraController(
-      // Use the widget.camera to initialize the controller
       widget.camera,
-      // Define the resolution to use
       ResolutionPreset.medium,
     );
 
@@ -58,6 +60,7 @@ class MainPageState extends State<MainPage> {
     super.dispose();
   }
 
+  // Send camera images to the endpoint
   void startSendingCameraImages() {
     // Start sending camera images x times per second
     Timer.periodic(Duration(milliseconds: (1000 ~/ _cameraFrequency)),
@@ -68,22 +71,53 @@ class MainPageState extends State<MainPage> {
       }
 
       // Take a picture and encode it to base64
-      final imageFile = await _controller.takePicture();
-      final imageBytes = await imageFile.readAsBytes();
-      final base64Image = base64Encode(imageBytes);
+      try {
+        final imageFile = await _controller.takePicture();
+        final imageBytes = await imageFile.readAsBytes();
+        final base64Image = base64Encode(imageBytes);
 
-      // Define the headers and body for the HTTP request
-      final headers = {'Content-Type': 'application/json'};
-      final body = jsonEncode({'image': base64Image});
+        // Define the headers and body for the HTTP request
+        final headers = {'Content-Type': 'application/json'};
+        final body = jsonEncode({'image': base64Image});
 
-      // Send the HTTP request to the endpoint
-      print("Posting to endpoint widh body length " +
-          base64Image.length.toString());
-      final response =
-          await http.post(Uri.parse(endpointUrl), headers: headers, body: body);
+        // Send the HTTP request to the endpoint
+        print("Posting to endpoint with body length " +
+            base64Image.length.toString());
+        final response = await http.post(Uri.parse(endpointUrl),
+            headers: headers, body: body);
 
-      // Print the HTTP response status code and body for debugging
-      print('HTTP response: ${response.statusCode} ${response.body}');
+        // Print the HTTP response status code and body for debugging
+        // print('HTTP response: ${response.statusCode} ${response.body}');
+
+        // Parse the HTTP response body as an integer
+        final responseBody = jsonDecode(response.body);
+        double responseNumber = responseBody['beepFrequency'].toDouble();
+
+        if (responseNumber != _beepFrequency) {
+          _beepFrequency = responseNumber;
+          _beepTimer.cancel();
+          if (responseNumber != 0) {
+            startBeeping();
+            print("Changing beeping to " + responseNumber.toString());
+          } else {
+            print("Stopping beeping");
+          }
+        }
+      } catch (e) {
+        // If camera capture still in progress, do nothing
+        print(e);
+      }
+    });
+  }
+
+  // Start beeping with a frequency defined by the endpoint
+  void startBeeping() {
+    // Start sending camera images x times per second
+    _beepTimer = Timer.periodic(
+        Duration(milliseconds: (1000 ~/ _beepFrequency)), (timer) async {
+      // Stop any currently playing beep sound and play a new one
+      await _audioPlayer.setPlaybackRate(2);
+      await _audioPlayer.play(AssetSource('beep-07a.wav'));
     });
   }
 
